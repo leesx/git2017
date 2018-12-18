9114f5ee281832189167857ea9a8f7fb31bd9d9f
图灵大盗 <leesx@users.noreply.github.com>
2018-12-18 17:57:10 +0800

    Update index.js

--- isomorphic-koa-react2/src/server/env/index.js
+++ isomorphic-koa-react2/src/server/env/index.js
@@ -1,15 +1,14 @@
-// 静态文件根路径
+
 const ASSETS_SERVICE_BASEURL = process.env.NODE_ENV === 'production' ?
-  'http://api.mendianbao.hualala.com' :
+  'http://api.leesx.com' :
   'http://127.0.0.1:3031'
 
-//API接口服务器
+
 const API_SERVICE_BASEURL = process.env.NODE_ENV === 'production' ?
-  'api.mendianbao.hualala.com' :
-  'dohko.api.mendianbao.hualala.com'
+  'api.leesx.com' :
+  'dev.api.leesx.com'
 
 const env = {
-  //脚本.min后缀
   HTTP_SCRIPT_SUFFIX: process.env.NODE_ENV === 'production' ? '.min' : '',
 
   // nodejs 服务端口
